d3fde0f887aceab833d56bfcdbc53e7dea149ec8
Alex Kubacki <usernotfound@gmail.com>
2026-03-11 23:27:06 -0600

    Ruby 4

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ Bundler.require(*Rails.groups)
 module TemplateRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 8.0
     config.exceptions_app = self.routes #self routes for exceptions
     # Configuration for the application, engines, and railties goes here.
     #
